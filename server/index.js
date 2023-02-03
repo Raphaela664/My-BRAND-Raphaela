479b6a42fb485ca602d7e3046d1fe9ffacddc0a9
Raphaela664 <mahoraphy02@gmail.com>
2023-02-03 15:01:29 +0200

    delete method added

--- server/index.js
+++ server/index.js
@@ -22,8 +22,31 @@ app.get('/blog',(req,res)=>{
 })
 app.get('/queries', async(req, res)=>{
     try{
-        const products = await Query.find({})
-        res.status(200).json(products);
+        const queries = await Query.find({})
+        res.status(200).json(queries);
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+app.get("/queries/:id", async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const query = await Query.findById(id)
+        res.status(200).json(query);
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+   
+})
+
+app.delete("/queries/:id", async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const query = await Query.findByIdAndDelete(id);
+        if(!query){
+            return res.status(404).json({message: `The message with ID: ${id} was not found`})
+        }
+        res.status(200).json(query);
     }catch(error){
         res.status(500).json({message:error.message})
     }
