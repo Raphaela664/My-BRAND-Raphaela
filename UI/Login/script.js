548d37d5362f424849502fafd6cdc7580d6da94f
Raphaela664 <mahoraphy02@gmail.com>
2023-02-19 20:40:30 +0200

    authentication integration done

--- UI/Login/script.js
+++ UI/Login/script.js
@@ -5,6 +5,7 @@
     messageElement.classList.remove("form__message--success", "form__message--error");
     messageElement.classList.add(`form__message--${type}`)
 }*/
+import  jwt_decode from "jwt-decode";
 const signBtn= document.getElementById('signSubmit');
 signBtn.addEventListener('click',signUser)
 const logBtn = document.getElementById('Login-button');
@@ -49,8 +50,15 @@ async function logUser(e){
      
     const logUser = await res.json();
     const token = logUser.token;
-    console.log(token);
+    decodedToken = jwt_decode(token);
+    if(decodedToken !== '63ef7ecb81ce07bf6698c2de'){
+        window.location.href('../dashboard.html')
+    }
+    else(
+        window.location.href('../index.html')
+    )
     
+
 }
 function setInputError(inputElement, message){
     inputElement.classList.add("form__input--error");
