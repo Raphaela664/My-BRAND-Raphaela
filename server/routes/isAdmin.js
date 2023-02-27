abb01785ab674d29efd01c63f13354598849d5a2
Raphaela664 <mahoraphy02@gmail.com>
2023-02-27 07:28:13 +0200

    admin issues

--- server/routes/isAdmin.js
+++ server/routes/isAdmin.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next)=>{
-    if(req.user._id !== 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjM2QxNjQyMDQ2ZDZiOWZhNTIxMmEiLCJpYXQiOjE2Nzc0NzUyNTB9.qF40F1DRBTABFNPDDDU3-MweJZU_d_k663OsUFSJ_70'){
+    if(req.user._id !== '63fc3d1642046d6b9fa5212a'){
         return res.status(401).send('Access belongs to Admin')
     }
     next();
